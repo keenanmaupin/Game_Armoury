2b2f5d84f4978611369c071994387882b48fe7ef
Bob <bobjacob23@yahoo.com>
2024-10-24 14:23:07 -0700

    changed file names and did index.ts

--- server/src/routes/api/gamesRoutes.ts
+++ server/src/routes/api/gamesRoutes.ts
@@ -1,31 +1,31 @@
 import express from 'express';
 import type { Request, Response } from 'express';
-import { Volunteer } from '../../models/index.js';
+import { Games } from '../../models/index.js';
 
 const router = express.Router();
 
 // GET /volunteers - Get all volunteers
 router.get('/', async (_req: Request, res: Response) => {
-  const volunteers = await Volunteer.findAll();
+  const volunteers = await Games.findAll();
   res.json(volunteers);
   // TODO: Update code to return all Volunteers
 });
 // GET /volunteers/:id - Get a volunteer by ID
 router.get('/:id', async (req: Request, res: Response) => {
-  const volunteers = await Volunteer.findByPk(req.params.id);
+  const volunteers = await Games.findByPk(req.params.id);
   res.json(volunteers);
   // TODO: Update code to return one Volunteer based on ID
 });
 // POST /volunteers - Create a new volunteer
 router.post('/', async (req: Request, res: Response) => {
-  const volunteers = await Volunteer.create(req.body);
+  const volunteers = await Games.create(req.body);
   res.json(volunteers);
   // TODO: Update code to create a Volunteer
 });
 
 // PUT /volunteers/:id - Update a volunteer by ID
 router.put('/:id', async (req: Request, res: Response) => {
-  const volunteers = await Volunteer.findByPk(req.params.id);
+  const volunteers = await Games.findByPk(req.params.id);
   if(volunteers) {
     await volunteers.update(req.body);
     res.json(volunteers);
@@ -35,7 +35,7 @@ router.put('/:id', async (req: Request, res: Response) => {
 
 // DELETE /volunteers/:id - Delete a volunteer by ID
 router.delete('/:id', async (req: Request, res: Response) => {
-  const volunteers = await Volunteer.findByPk(req.params.id);
+  const volunteers = await Games.findByPk(req.params.id);
   if(volunteers) {
     await volunteers.destroy();
     res.json(volunteers);
@@ -43,4 +43,4 @@ router.delete('/:id', async (req: Request, res: Response) => {
   // TODO: Update code to delete Volunteer based on ID
 });
 
-export { router as volunteerRouter };
+export { router as gamesRouter };
--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import { gamesRouter } from './gamesRoutes.js';
+import { userRouter } from './usersRoutes.js';
+
 
 const router = Router();
 
-import { gamesRouter } from './games.js';
-import { usersRouter } from './users.js';
+// import { gamesRouter } from './games.js';
+// import { usersRouter } from './users.js';
 
-const router = Router();
+router.use('/games', ) 
+
+export default router;
