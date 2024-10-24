0b5c0e63a4c75ea81170370582072b7be3b11b9c
RoracJ <roracjohnson@gmail.com>
2024-10-24 13:30:24 -0700

    LoginPage.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,3 +8,13 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LoginPage from './pages/LoginPage';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <LoginPage />
+    </React.StrictMode>
+);
