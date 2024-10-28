f12b93be75271cbac087f4d6e441d190fbba6f2d
Kenjy Jap <kenhiebusiness@gmail.com>
2024-10-28 11:35:54 -0700

    Client side pathing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,14 @@
-import { useState } from 'react';
-import UserForm from './components/UserForm';
+import { Outlet } from 'react-router-dom';
+// import Nav from './components/Nav';
 
 function App() {
-  const [result, setResult] = useState<string | null>(null);
-
-  const handleFormSubmit = async (query: string) => {
-    try {
-      // Send the query to the backend API endpoint
-      const response = await fetch('http://localhost:5000/api/fetchData', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ query }),
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to fetch data');
-      }
-
-      const data = await response.json();
-      setResult(data.result); // Assuming your backend sends the result as `data.result`
-    } catch (error) {
-      console.error('Error:', error);
-      setResult('Error fetching data');
-    }
-  };
-
   return (
-    <div>
-      <h1>Search API</h1>
-      <UserForm onSubmit={handleFormSubmit} />
-      {result && <p>Result: {result}</p>}
-    </div>
+    <>
+      {/* <Nav /> */}
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 }
 
