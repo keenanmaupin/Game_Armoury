9dec95555a2aaf3ed23e480cda3d03c8708ae3fe
Kenjy Jap <kenhiebusiness@gmail.com>
2024-10-30 14:05:49 -0700

    added path

--- server/dist/server.js
+++ server/dist/server.js
@@ -2,12 +2,16 @@ const forceDatabaseRefresh = false;
 import express from 'express';
 import sequelize from './config/connection.js';
 import routes from './routes/index.js';
+import path from 'path';
 const app = express();
 const PORT = process.env.PORT || 3001;
 // Serves static files in the entire client's dist folder
 app.use(express.static('../client/dist'));
 app.use(express.json());
 app.use(routes);
+app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
     app.listen(PORT, () => {
         console.log(`THE Server is UP and JUMPIN on port ${PORT}!!!`);
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ const forceDatabaseRefresh = false;
 import express from 'express';
 import sequelize from './config/connection.js';
 import routes from './routes/index.js';
+import path from 'path'
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +13,10 @@ app.use(express.static('../client/dist'));
 
 app.use(express.json());
 app.use(routes);
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 
 sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
   app.listen(PORT, () => {
