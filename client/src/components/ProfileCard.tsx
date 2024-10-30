4ace85b4e5704d9bfe335baf3fc2d2a84f61d87a
Bob <bobjacob23@yahoo.com>
2024-10-29 19:32:49 -0700

    got the library working and addded css

--- client/src/components/ProfileCard.tsx
+++ client/src/components/ProfileCard.tsx
@@ -18,37 +18,31 @@ const ProfileCard = ({ currentGame }: GameCardProps) => {
       {currentGame?.name ? (
         <section className="col-12 col-md-6 col-lg-3 mb-4">
           <div className="card h-100" style={{ width: "30rem" }}>
-            {/* Top image with full width */}
             <img
               className="card-img-top"
               style={{ height: "200px", objectFit: "cover" }}
-              src={currentGame.background_image ? currentGame.background_image : "../assets/pexels-atomlaborblog-776092.jpg"}
+              src={currentGame.background_image || "../assets/pexels-atomlaborblog-776092.jpg"}
               alt={currentGame.name}
             />
-
             <div className="card-body">
-              {/* Title in its own row and centered */}
               <div className="row">
                 <h2 className="card-title text-center w-100 pb-2">{currentGame.name}</h2>
               </div>
-
-              {/* Content with reduced padding between <p> tags */}
               <div className="row">
                 <div className="col-6">
                   <p className="card-text mb-1">
-                    <strong>Created by:</strong> {currentGame.developers.length ? currentGame.developers[0].name : "Anonymous"}
+                    <strong>Created by:</strong> {currentGame.developers?.length ? currentGame.developers[0].name : "Anonymous"}
                   </p>
                   <p className="card-text mb-1">
-                    <strong>Platforms:</strong> {currentGame.platforms.length ? currentGame.platforms[0].platform.name : "..."}
+                    <strong>Platforms:</strong> {currentGame.platforms?.length ? currentGame.platforms[0].platform.name : "N/A"}
                   </p>
                   <p className="card-text mb-1">
                     <strong>Released:</strong> {currentGame.released}
                   </p>
                   <p className="card-text mb-1">
-                    <strong>Genre:</strong> {currentGame.genres.length ? currentGame.genres[0]?.name : "..."}
+                    <strong>Genre:</strong> {currentGame.genres?.length ? currentGame.genres[0]?.name : "N/A"}
                   </p>
                 </div>
-
                 <div className="col-6">
                   <p className="card-text mb-1">
                     <strong>Plot:</strong> {currentGame.description_raw.slice(0, 100)}
